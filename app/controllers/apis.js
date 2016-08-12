e83eb753a4c4700d68b0c827f11910454f64c21c
Bryce Kolton <bbkolton@uw.edu>
2016-08-12 20:59:50 +0000

    Implemented web view of REST API

--- app/controllers/apis.js
+++ app/controllers/apis.js
@@ -9,8 +9,23 @@ module.exports = function(app) {
 for (prop in db) {
 	if (prop.toLowerCase() != "sequelize") {
 		constructRestApi(router, prop);
+		constructWebView(router, prop); //>>>???????
 	}
 }
+
+
+function constructWebView(router, model) {
+	var url = model.toLowerCase();
+
+
+}
+
+
+
+
+
+
+
 
 router.get('/api/requester', function(req, res) {
 	res.render('requester');
@@ -23,19 +38,43 @@ function constructRestApi(router, model) {
 
 	//for get requests with a specific ID
 	router.get('/api/'+ url +'/:id', function(req, res) {
-		db[model].findById(req.params.id)
-		.then(function(mod) {
-			res.json(mod);
+		getId(req, function(row) {
+			res.json(row);
 		});
 	});
 
+	router.get('/view/'+ url +'/:id', function(req, res) {
+		getId(req, function(row) {
+			console.log(row.dataValues)
+			res.render('rowviewer', {row: row.dataValues});
+		})
+	});
+
+	function getId(req, callback) {
+		db[model].findById(req.params.id)
+		.then(function(row) {
+			callback(row)
+		});
+	}
+
 	//for get requests of a whole model
 	router.get('/api/'+ url, function(req, res) {
+		getModel(req, function(mod) {res.json(mod)});
+	});
+
+	router.get('/view/'+ url, function(req, res) {
+		getModel(req, function(mod) {
+			res.render('modelviewer', {model: mod})
+		});
+	});
+
+	function getModel(req, callback) {
 		db[model].findAll()
 		.then(function(mod) {
-			res.json(mod);
-		})
-	})
+			console.log(mod)
+			callback(mod);
+		});
+	}
 
 	//for post requests to a whole model
 	router.post('/api/'+ url, function(req, res) {
